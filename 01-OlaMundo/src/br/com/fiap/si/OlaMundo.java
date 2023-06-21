9bb5dfefb4444c05f405c9a5ba56838f11850d11
wowilliam <wo.william@hotmail.com>
2023-06-21 20:26:44 -0300

    calcula o IMC

---
+++ 01-OlaMundo/src/br/com/fiap/si/OlaMundo.java
@@ -0,0 +1,21 @@
+package br.com.fiap.si;
+
+public class OlaMundo {
+	public static void main (String[] args) {
+		
+		
+		int idade = 10;
+		double preco =10.0;
+		char genero = 'M';
+		boolean maiorIdade = false;
+		
+		System.out.println(idade);
+		System.out.println(preco);
+		System.out.println(genero);
+		System.out.println(maiorIdade);
+		
+		System.out.println(" FIAP - Olá Mundo!");
+		
+	}
+
+}
