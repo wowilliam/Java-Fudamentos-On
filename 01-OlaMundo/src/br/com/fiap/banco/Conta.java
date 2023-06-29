7f53cbf3823fe3128d5c045bef665b94511126fe
wowilliam <wo.william@hotmail.com>
2023-06-28 21:11:02 -0300

    Classe, Métodos, Atributos

--- 01-OlaMundo/src/br/com/fiap/banco/Conta.java
+++ 01-OlaMundo/src/br/com/fiap/banco/Conta.java
@@ -2,30 +2,42 @@ package br.com.fiap.banco;
 
 public class Conta {
 
-	int numero;
-	int agencia;
+	private int numero;
+	private int agencia;
 	private double saldo;
 
-	public Conta(){
+	public Conta() {
 
 	}
 
-	public Conta(int numero, int agencia, double saldo){
-		this.numero = numero;
-		this.agencia = agencia;
+	public Conta(int numero, int agencia, double saldo) {
+		this.setNumero(numero);
+		this.setAgencia(agencia);
 		this.setSaldo(saldo);
 	}
 
-	public void depositar(double valor){
-		this.setSaldo(this.getSaldo() + valor);
+	public void depositar(double valor) {
+		saldo = saldo + valor;
 	}
 
-	public void retirar(double valor){
-		this.setSaldo(this.getSaldo() - valor);
+	public void retirar(double valor) {
+		saldo = saldo - valor;
 	}
 
-	public double verificarSaldo(){
-		return getSaldo();
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
 	}
 
 	public double getSaldo() {
--- 01-OlaMundo/src/br/com/fiap/banco/teste/Teste.java
+++ 01-OlaMundo/src/br/com/fiap/banco/teste/Teste.java
@@ -3,15 +3,22 @@ package br.com.fiap.banco.teste;
 import br.com.fiap.banco.Conta;
 
 public class Teste {
+
 	public static void main(String[] args) {
-		
+
 		Conta cc = new Conta();
-        cc.setSaldo(1000);
-        cc.depositar(100);
-        cc.verificarSaldo();
+		cc.depositar(50);
+		cc.setAgencia(123);
+		cc.setNumero(321);
 
-        double valor = cc.getSaldo();
-        System.out.println(cc.getSaldo());
-    }
+		cc.depositar(100);
 
+		System.out.println(cc.getSaldo());
+
+		Conta poupanca = new Conta(111,222,1000);
+
+		poupanca.retirar(50);
+
+		System.out.println(poupanca.getSaldo());
+	}
 }
