c0ef1775b42be74f6cd289cd10d1691beeb23234
wowilliam <wo.william@hotmail.com>
2023-07-05 20:04:06 -0300

    Herança

---
+++ 01-OlaMundo/src/br/com/fiap/tds/ContaCorrente.java
@@ -0,0 +1,10 @@
+package br.com.fiap.tds;
+
+public class ContaCorrente extends Conta {
+  private String tipo;
+
+  public ContaCorrente (int numero, String tipo){
+    super(numero);
+    this.tipo = tipo;
+  }
+}
